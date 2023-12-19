9b98508d1522738935f7f8e5505d95ed0194ce93
heejin5484 <heejin990308@gmail.com>
2023-12-19 16:55:51 +0900

    test
    test

--- Assets/Scripts/transparent.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class transparent : MonoBehaviour
-{
-    [Range(0f,1f)]
-    public float transparancy;
-}
