b4638843556b4aaa4f93f84d4da8f62397b67175
heejin5484 <heejin990308@gmail.com>
2024-02-03 23:55:34 +0900

    auto ui

--- Assets/RHJ/Scripts/GoalCheck.cs
+++ Assets/RHJ/Scripts/GoalCheck.cs
@@ -11,7 +11,6 @@ public class GoalCheck : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log("°æº¸");
             TargetEvent.SetActive(true);
         }
 
