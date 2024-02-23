b33b1b9ce8eb36ca5c226850ef2ca91d748f1032
heejin5484 <heejin990308@gmail.com>
2024-02-23 19:42:28 +0900

    feat: Grabbing Object
    - MouseLook.cs Start부분 수정 (공용파일) 마우스 숨김 해제
    
    - 레이캐스트로 오브젝트 감지시 material 색상 변경 (후에 쉐이더 작성해서 glow같은거로 바꾸기)
    - 오브젝트 좌클릭으로 장착, 우클릭으로 해제 임시코드 작성

---
+++ Assets/RHJ/Scripts/GrabObject.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using UnityEngine;
+
+public class GrabObject : MonoBehaviour
+{
+    public Transform Player;
+    public bool grabbing;
+    public Transform ObjectOnHand_base;
+    public Material highlightMaterial;
+
+    private Material originalMaterial;
+
+    void Start()
+    {
+        grabbing = false;
+        originalMaterial = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+        {
+            if (hit.transform == transform)
+            {
+                OnRaycastEnter();
+            }
+            else
+            {
+                OnRaycastExit();
+            }
+        }
+        else
+        {
+            OnRaycastExit();
+        }
+
+        if (grabbing)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                grabbing = false;
+                transform.parent = null;
+                gameObject.GetComponent<Rigidbody>().isKinematic = false;
+                GetComponent<Renderer>().material = originalMaterial;
+            }
+        }
+
+    }
+
+    void OnRaycastEnter()
+    {
+        //Debug.Log("Raycast Enter");
+        if (highlightMaterial != null && !grabbing)
+        {
+            GetComponent<Renderer>().material = highlightMaterial;
+        }
+
+        if (Player)
+        {
+            float dist = Vector3.Distance(Player.position, transform.position);
+            Debug.Log(dist);
+            if (dist < 4)
+            {
+                //Debug.Log("Dist < 4");
+                if (grabbing == false)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        //Debug.Log("Pushed");
+                        transform.parent = ObjectOnHand_base;
+                        gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                        transform.localPosition = Vector3.zero;
+                        transform.localRotation = ObjectOnHand_base.localRotation;
+                        grabbing = true;
+                    }
+                }
+            }
+        }
+    }
+
+    void OnRaycastExit()
+    {
+        //Debug.Log("Raycast Exit");
+        GetComponent<Renderer>().material = originalMaterial;
+    }
+}
